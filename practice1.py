c10c62134e5c76ac8fb4bf48495d04164071c274
mairahussain4854-svg <mairahussain4854@gmail.com>
2025-10-22 23:25:24 +0500

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,10 +1,10 @@
 temperature=float(input("enter the temperature:"))
-method=int(input("enter 1 for centi to feren and enter 2 to convert feren to centi:"))
+method=int(input("enter 1 for centi to fahren and enter 2 to convert feren to centi:"))
 if(method==1):
-    F=(temperature * 9/5)+32
-    print(F)
+    result=(temperature * 9/5)+32
+    print(result)
 elif(method==2):
-    C = [(temperature-32)*5]/9
-    print(C)
+    result = [(temperature-32)*5]/9
+    print(result)
 else:
     print("invalid entry")
